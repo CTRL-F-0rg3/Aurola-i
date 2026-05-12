66ef861d3054b54f9ecc18b811f4b39cf3b0b7db
ctrl-f-0-rg3 <ctrlf0rge@protonmail.com>
2026-05-12 12:36:37 +0200

    0.0.0.2

---
+++ logicwritergramar.py
@@ -0,0 +1,21 @@
+#
+
+# rzeczownik (noun)
+
+
+
+# czasownik (verb)
+
+# przymiotnik (adjective)
+
+# przysłówek / okolicznik (adverb)
+
+# zaimek (pronoun)
+
+# przyimek (preposition)
+
+# spójnik (conjunction)
+
+# liczebnik (numeral)
+
+# wykrzyknik (interjection)
